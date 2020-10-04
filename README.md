746f927e2ce2f223fd95f8d605d6fd543bf5397a
Tarannu <tarannum.aiub@gmail.com>
2020-10-04 02:51:03 -0400

    testing readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This repository contains a budget web application. Written in JS.
+>Budget Application to track expenses
--- starter/app.js
+++ starter/app.js
@@ -1,3 +1,5 @@
+const { table } = require("console");
+
 //selectors
 const descriptionInput=document.querySelector('add__description');
 const valueInput=document.querySelector('.add__value');
@@ -15,22 +17,31 @@ function addToList(e){
     e.preventDefault(); 
 
     //create div
-    const listDiv=document.createElement('div');
-    listDiv.classList.add('list');
+    const tableDiv=document.createElement('div');
+    tableDiv.classList.add('incomeDiv');
 
-    //create list
-    const newValue=document.createElement('table');
-    newValue.innerText=valueInput.value;
-    newValue.classList.add("income-item");
-    listDiv.appendChild(newValue);
+    //create table
+    const incomeTable=document.createElement('table');
+    incomeTable.classList.add('incomTable')
 
-
+    //create table elements
+    const row=incomeTable.insertRow(0);
+    const newDes=document.createElement('td');
+    const newValue=document.createElement('td');
     
+    newDes=row.insertCell(0);
+    newDes.innerText=descriptionInput.value;
+
+    newValue=row.insertCell(1);
+    newValue.innerText=valueInput.value;
+    
+    row.classList.add('incomeRow');
+    
+    incomeTable.appendChild(row);
+    tableDiv.appendChild(incomeTable);
 
     //append to list
-    incomeList.appendChild(listDiv);
+    incomeList.appendChild(tableDiv);
     //document.getElementsByClassName("income__container").innerHTML =valueInput.value ;
     valueInput.value="";
-
-
 }
