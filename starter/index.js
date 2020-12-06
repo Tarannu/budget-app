4ab1a0cc46f247ed2a74362fdbaa2226f238583e
taranum islam <tarannum.aiub@gmail.com>
2020-12-05 21:49:08 -0500

    adding from new folder from pc

--- starter/index.js
+++ starter/index.js
@@ -3,29 +3,26 @@ var incomeTable=document.getElementById('income__table');//income table
 var descriptionInput=document.getElementById('description');
 var numberInput=document.getElementById('value');
 var addButton=document.getElementById('add__btn');
-var selectInput=document.getElementById('add__type').value;
+var selectInput=document.getElementById('add__type');
 var expenseTable=document.getElementById('expenses__table');//expenses table
 var budgetIncome=document.getElementsByClassName('budget__income--value');
 //creating funtions
 function creatingTable(){
     console.log("Creating Table Function Started");
 
-    console.log("the function is here before if")
     var htmlContent=`<tr><td>${descriptionInput.value}</td><td>${numberInput.value}</td><td><button class="delete-button"><i class="ion-ios-close-outline"></i></button></td></tr>`;
-    // if(selectInput==='inc'){
-    //     console.log("if inc is entered");
+    
+    if(selectInput==='inc'){
+        console.log("if inc is entered");
         incomeTable.insertAdjacentHTML('beforeend',htmlContent);
-    // } else if(selectInput==='exp'){
-    //     //addToExpense(); //add to expense table
-            //console.log("if exp is entered")
-         //expenseTable.insertAdjacentElement('beforeend',htmlContent);
-    if(selectInput=='inc'){
-        var check='inc';
+
+    } 
+    else if(selectInput==='exp'){
+
+        //addToExpense(); //add to expense table
+        console.log("if exp is entered")
+        expenseTable.insertAdjacentElement('beforeend',htmlContent);
     }
-    else {
-        check='exp';
-    }
-    console.log(check);
     
     console.log("the function is here after if")
     function addToBudget(){
