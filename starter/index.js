e0a9e3fd007054a43d247957de7907a0f6ef4bdf
taranum islam <tarannum.aiub@gmail.com>
2020-12-06 02:03:28 -0500

    adding delete button to income table

--- starter/index.js
+++ starter/index.js
@@ -8,8 +8,8 @@ var expenseTable=document.getElementById('expense__table');//expense table
 var totalBudget=document.getElementById('budget__value');
 var budgetIncomeDiv=document.getElementById('budget__income--value');
 var budgetExpenseDiv=document.getElementById('budget__expenses--value');
-//var incomePercentageDiv=document.getElementById('budget__income--percentage');
 var expensePercentageDiv=document.getElementById('budget__expenses--percentage');
+var monthDiv=document.getElementById('budget__title--month');
 
 //Setting the budget values for the top part
 var budget=0;
@@ -17,15 +17,37 @@ var budgetIncome=0;
 var budgetExpense=0;
 
 //creating functions
+function addMonth(){
+    var month=[];
+    month[0]='January';
+    month[1]='February';
+    month[2]='March';
+    month[3]='April';
+    month[4]='May';
+    month[5]='June';
+    month[6]='July';
+    month[7]='August';
+    month[8]='September';
+    month[9]='October';
+    month[10]='November';
+    month[11]='December';
+    
+    var date=new Date();
+    var monthValue=month[date.getMonth()];
+    monthDiv.innerHTML=monthValue+" "+date.getFullYear();
+}
+
 function creatingTable(){
     console.log("Creating Table Function Started");
+
+    if(selectInput.value==='inc') var sign="+";
+    else if(selectInput.value==='exp') var sign="-";
     
-    
-    var htmlContent=`<tr><td>${descriptionInput.value}</td><td>${numberInput.value}</td><td><button class="delete-button"><i class="ion-ios-close-outline"></i></button></td></tr>`;
+    var htmlContent=`<tr><td>${descriptionInput.value}</td><td>${sign} ${numberInput.value}</td><td><button class="delete-button"><i class="ion-ios-close-outline"></i></button></td></tr>`;
     
     
     if(selectInput.value==='inc'){
-
+        
         console.log("if inc is entered");
         incomeTable.insertAdjacentHTML('beforeend',htmlContent);
         budget+=parseInt(numberInput.value);
@@ -39,34 +61,45 @@ function creatingTable(){
         budgetExpense+=parseInt(numberInput.value);
         budget=budget-budgetExpense;
     }
+    else {
+        window.alert("Please select the sign + or - ");
+        
+    }
     
     
     //Adding budget calculation at top of the page
     function addToBudget(){
         totalBudget.innerHTML=budget;
-        budgetIncomeDiv.innerHTML=budgetIncome;
-        budgetExpenseDiv.innerHTML=budgetExpense;
+        budgetIncomeDiv.innerHTML="+ "+budgetIncome;
+        budgetExpenseDiv.innerHTML="- "+budgetExpense;
     }
 
     addToBudget();
     //adding percentage to top
     function addPercentage(){
         var percentageOfExpense=parseFloat((budgetExpense/budgetIncome)*100);
-        expensePercentageDiv.innerHTML=percentageOfExpense+" %";
+        
+        expensePercentageDiv.innerHTML=Math.round(percentageOfExpense)+" %";
+
         
     }
     addPercentage();
-
-    // function addCalender{
-
-
-    // }
-    // addCalender();
     
     descriptionInput.value='';
     numberInput.value='';
 
 }
+//delete row and update budget
+function deleteRow(e){
+    if(e.target.className==='delete-button'){
+        var row=e.target.parentElement.parentElement;
+        e.target.parentElement.parentElement.remove(row);
+    }
+    
+}
 
-//create event handler
+//create event handler and calling functions
+addMonth();
 addButton.addEventListener('click',creatingTable);
+document.getElementById('income__table').addEventListener('click',deleteRow);
+document.getElementById('expense__table').addEventListener('click',deleteRow);
